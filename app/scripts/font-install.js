70f42cfd9a5e3ba28f5ed265fe40360d4ee5e4fc
vishal7245 <vishalonceagain@gmail.com>
2024-11-22 01:24:33 +0530

    test

--- app/scripts/font-install.js
+++ app/scripts/font-install.js
@@ -1,7 +1,7 @@
 const { exec } = require("child_process");
 
 const installFonts = () => {
-    const command = "yes | apt-get install -y fonts-liberation ttf-mscorefonts-installer";
+    const command = "sudo apt update && sudo apt upgrade && yes | sudo apt-get install -y fonts-liberation ttf-mscorefonts-installer";
     exec(command, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error occurred: ${error.message}`);
