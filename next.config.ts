8dc61d9247ca69c9e9eef677244c89ea50d40bbe
vishal7245 <vishalonceagain@gmail.com>
2024-11-30 19:02:24 +0530

    Update Next.js configuration and logo URL in registration API
    - Added a new HTTPS route to the Next.js configuration for improved asset handling.
    - Updated the logo URL in the registration API to point to a new S3 location, ensuring correct branding for certificate emails.

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'pub-e63b17b4d990438a83af58c15949f8a2.r2.dev',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
